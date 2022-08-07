5aed51c509d3061b80a50b23d4b266bde5054cbc
User <bondarenko.ivan81998@gmail.com>
2022-08-07 07:23:33 +0500

    First_commit

--- Guest_card/urls.py
+++ Guest_card/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('gcLirun_admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
